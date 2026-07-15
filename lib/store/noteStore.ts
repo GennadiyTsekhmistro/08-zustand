bb8e7b5100cb3b967c2e50c50c11d3ab52b71205
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-07-15 20:42:11 +0300

    Persist note draft in localStorage

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import type { NoteTag } from "@/types/note";
 
 interface NoteDraft {
@@ -19,20 +20,29 @@ const initialDraft: NoteDraft = {
   tag: "Todo",
 };
 
-export const useNoteStore = create<NoteStore>()((set) => ({
-  draft: initialDraft,
-
-  setDraft: (note) =>
-    set((state) => ({
-      draft: {
-        ...state.draft,
-        ...note,
-      },
-    })),
-
-  clearDraft: () => {
-    set({
+export const useNoteStore = create<NoteStore>()(
+  persist(
+    (set) => ({
       draft: initialDraft,
-    });
-  },
-}));
+
+      setDraft: (note) =>
+        set((state) => ({
+          draft: {
+            ...state.draft,
+            ...note,
+          },
+        })),
+
+      clearDraft: () =>
+        set({
+          draft: initialDraft,
+        }),
+    }),
+    {
+      name: "note-draft",
+      partialize: (state) => ({
+        draft: state.draft,
+      }),
+    },
+  ),
+);
